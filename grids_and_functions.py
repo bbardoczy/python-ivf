9b552bbed23eb4050dcba4991401a540e57ea83c
Egor Kozlov <egkozlov@nes.ru>
2019-08-13 16:13:04 +0300

    Uses model class

---
+++ grids_and_functions.py
@@ -0,0 +1,156 @@
+# this contains choices for grids and functional forms
+import numpy as np
+from numba import njit
+
+class setupClass:
+    def __init__(self,pars):
+        self.pars = pars
+        self.define_agrid()
+        self.define_u()
+        self.define_trends()
+        self.define_grids_and_transitions()
+        self.define_labor_income()
+        self.define_transitions()
+        
+        
+        
+    def define_agrid(self):
+        from generate_grid import generate_agrid
+        self.agrid = generate_agrid(n = self.pars['na'], amax=self.pars['amax'])  
+
+    
+    def define_u(self):
+        import uenv
+        
+        sigma, u_kid_c, u_kid_add = self.pars['sigma'], self.pars['u_kid_c'], self.pars['u_kid_add']
+    
+        uselog = True if sigma == 1 else False
+        
+        if uselog:        
+            factor = 1
+            @njit
+            def u_nok(x):
+                return np.log(x)        
+            @njit
+            def u_k(x):
+                return np.log(x) + u_kid_c + u_kid_add
+            
+        else:
+            factor = np.exp(u_kid_c*(1-sigma))        
+            @njit
+            def u_nok(x: np.float64) -> np.float64:
+                return (x**(1-sigma))/(1-sigma)        
+            @njit
+            def u_k(x: np.float64) -> np.float64:
+                return factor*(x**(1-sigma))/(1-sigma) + u_kid_add
+            
+        
+        self.u =  {
+                'No children':    u_nok,
+                'One child, out': u_k,
+                'One child, in':  u_k
+             }
+        
+        self.mu = {
+                'No children':    lambda x  : (x**(-sigma)),
+                'One child, out': lambda x  : factor*(x**(-sigma)),
+                'One child, in':  lambda x  : factor*(x**(-sigma))
+             }
+        
+        self.mu_inv = {'No children':    lambda MU : (MU**(-1/sigma)),  
+                  'One child, out': lambda MU : ((MU/factor)**(-1/sigma)),
+                  'One child, in':  lambda MU : ((MU/factor)**(-1/sigma))
+                 }
+            
+        
+        self.ue = {
+                'No children':    uenv.create(self.u['No children'],False),
+                'One child, out': uenv.create(self.u['One child, out'],False),
+                'One child, in':  uenv.create(self.u['One child, in'],False)
+             }
+        
+
+    @staticmethod   
+    def define_polynomial_trend(coefs,ts,fun=lambda x: x):
+        T = np.ones_like(ts)
+        trend = np.zeros_like(ts)
+        
+        for a in coefs:
+            trend += a*T
+            T     *= ts
+            
+        return fun(trend)
+    
+    def define_trends(self,exp_g=True,normalize_T=True):
+        
+        pars = self.pars
+        
+        zcoefs = [pars['a_z0'],pars['a_z1'],pars['a_z2']]
+        gcoefs = [pars['a_g0'],pars['a_g1']]
+        
+        trange = np.arange(0,pars['T'])
+        
+        if normalize_T:
+            trange = trange/np.mean(trange)
+        self.ztrend = self.define_polynomial_trend(zcoefs,trange)
+        
+        self.gtrend = self.define_polynomial_trend(gcoefs,trange)
+        
+        if exp_g:
+            self.gtrend = np.exp(self.gtrend)
+            
+    
+    
+    
+    def define_grids_and_transitions(self):
+        # this takes whole pars as I did not figure out a better way
+        
+        
+        from generate_grid import generate_zgs
+        
+        pars = self.pars
+        
+        a = dict(sigma_z_init=pars['sigma_z_init'],sigma_z=pars['sigma_z'],nz=pars['nz'],
+                     sigma_g=pars['sigma_g'],rho_g=pars['rho_g'],ng=pars['ng'],smin=0,smax=pars['smax'],ns=pars['ns'],
+                     T=pars['T'],mult=self.gtrend)
+        
+        zgs_GridList_nok,   zgs_MatList_nok  = generate_zgs(**a)
+        zgs_GridList_k,     zgs_MatList_k  = generate_zgs(**a,fun = lambda g : np.maximum( g - pars['g_kid'], 0 ) )
+        
+        self.zgs_Grids =    {
+                            'No children': zgs_GridList_nok,
+                            'One child, out': zgs_GridList_k,
+                            'One child, in': zgs_GridList_k
+                            }
+        
+        self.zgs_Mats  =    {
+                            'No children': zgs_MatList_nok,
+                            'One child, out': zgs_MatList_k,
+                            'One child, in': zgs_MatList_k
+                            }
+        
+
+    
+    def define_labor_income(self):
+        
+        pars = self.pars
+        
+        self.labor_income = {
+                        'No children':    lambda grid, t : np.exp(grid[:,0] + grid[:,2] + self.ztrend[t]).T,
+                        'One child, out': lambda grid, t : pars['phi_out']*np.exp(grid[:,0] + grid[:,2] - pars['z_kid']  + self.ztrend[t]).T,
+                        'One child, in':  lambda grid, t : pars['phi_in']*np.exp(grid[:,0] + grid[:,2]  - pars['z_kid']  + self.ztrend[t]).T
+                       }
+        
+
+    
+    def define_transitions(self):
+        pars = self.pars
+
+
+        from between_states import shock, choice
+        new_baby = shock(['One child, out',"One child, in"],[1,0])
+        self.transitions = {'No children':    choice(["No children",new_baby],pars['eps']),
+                         'One child, out':    shock(["One child, out","One child, in"],[1-pars['pback'],pars['pback']]),
+                          'One child, in':    choice(["One child, in"],pars['eps'])}
+    
+    
--- main.py
+++ main.py
@@ -6,75 +6,90 @@ Created on Sat Jul 27 13:39:24 2019
 
 @author: Egor Kozlov
 
-This thing runs the main calculations for IVF project. Nothing important
-here now
+This thing runs the main calculations for IVF project.
 
 """
 
-from generate_grid import generate_zgs, generate_agrid
 import matplotlib.pyplot as plt
 import numpy as np
-#from vf_tools import smooth_max, smooth_p0
-from numba import njit
-from valueFunctions import valuefunction, grid_dim_linear, grid_dim_discrete
-from vf_iterators import Vnext_egm
-from between_states import ev_emu, shock, choice
+from between_states import ev_emu
 
 
-def vpack(Vcs,agrid,zgsgrid,time,description=""):
-    
-    Vout = valuefunction([grid_dim_linear(agrid),grid_dim_discrete(zgsgrid)],{'V':Vcs[0],'c':Vcs[1],'s':Vcs[2]},time,description)
-    return Vout
-    
-def define_u(sigma,u_kid_c,u_kid_add):
-    import uenv
 
-    uselog = True if sigma == 1 else False
-    
-    if uselog:        
-        factor = 1
-        @njit
-        def u_nok(x):
-            return np.log(x)        
-        @njit
-        def u_k(x):
-            return np.log(x) + u_kid_c + u_kid_add
+class model:
+    def __init__(self,pars,iterator_name="EGM"):
+        from grids_and_functions import setupClass
+        self.setup = setupClass(pars)
+        self.iterator_name = iterator_name
+        self.initialize, self.iterate = self.get_iterator(iterator_name)
         
-    else:
-        factor = np.exp(u_kid_c*(1-sigma))        
-        @njit
-        def u_nok(x: np.float64) -> np.float64:
-            return (x**(1-sigma))/(1-sigma)        
-        @njit
-        def u_k(x: np.float64) -> np.float64:
-            return factor*(x**(1-sigma))/(1-sigma) + u_kid_add
         
-    
-    u =  {
-            'No children':    u_nok,
-            'One child, out': u_k,
-            'One child, in':  u_k
-         }
-    
-    mu = {
-            'No children':    lambda x  : (x**(-sigma)),
-            'One child, out': lambda x  : factor*(x**(-sigma)),
-            'One child, in':  lambda x  : factor*(x**(-sigma))
-         }
-    
-    mu_inv = {'No children':    lambda MU : (MU**(-1/sigma)),  
-              'One child, out': lambda MU : ((MU/factor)**(-1/sigma)),
-              'One child, in':  lambda MU : ((MU/factor)**(-1/sigma))
-             }
+    # this sets up iterators    
+    def get_iterator(self,name):
+        if name == "EGM":
+            
+            from vf_iterators import Vnext_egm
+            s = self.setup
+            def iterate(desc,t,EV,EMU):
+                gri, ma = s.zgs_Grids[desc][t], s.zgs_Mats[desc][t]            
+                return Vnext_egm(s.agrid,s.labor_income[desc](gri,t),EV,EMU,ma,s.pars['R'],s.pars['beta'],u=s.u[desc],mu_inv=s.mu_inv[desc],uefun=s.ue[desc])
         
+            def initialize(desc,T):
+                return iterate(desc,T,None,None)
+            
+        elif name == "VFI":
+            
+            from vf_iterators import Vnext_vfi
+            s = self.setup
+            def iterate(desc,t,EV,EMU):
+                gri, ma = s.zgs_Grids[desc][t], s.zgs_Mats[desc][t]            
+                return Vnext_vfi(s.agrid,s.labor_income[desc](gri,t),EV,EMU,ma,s.pars['R'],s.pars['beta'],u=s.u[desc],mu_inv=s.mu_inv[desc],uefun=s.ue[desc])
+            def initialize(desc,T):
+                return iterate(desc,T,None,None)
+            
+        else:
+            raise Exception('Cannot find iterator!')
+        
+        return initialize, iterate
+        
+        
+        
+    def compute_V(self):
+        self.V =  [{ 'No children':None, 'One child, out':None, 'One child, in':None }]*self.setup.pars['T']
+        self.descriptions = [*self.V[0]]
+        
+        # this part is backward iteration
+        for t in reversed(range(T)):
+            
+            for desc in self.descriptions:
+                if t == T-1:
+                    Vcs = self.initialize(desc,t)  
+                else:
+                    s = self.setup
+                    ma = s.zgs_Mats[desc][t]            
+                    integrate = lambda V : np.dot(V,ma.T)            
+                    Vcomb, MU_comb = ev_emu(self.V[t+1],s.transitions[desc],mu=s.mu[desc])                
+                    EV, EMU  = integrate(  Vcomb  ), integrate(  MU_comb  )  
+                    Vcs = self.iterate(desc,t,EV,EMU)                
+                    assert np.all(MU_comb > 0)  
+        
+                self.V[t][desc] = self.vpack(Vcs,T-1,desc)
+            
+            
+                
     
-    ue = {
-            'No children':    uenv.create(u['No children'],False),
-            'One child, out': uenv.create(u['One child, out'],False),
-            'One child, in':  uenv.create(u['One child, in'],False)
-         }
+    def vpack(self,Vcs,time,desc):
+        from valueFunctions import valuefunction, grid_dim_linear, grid_dim_discrete
+        agrid = self.setup.agrid
+        zgsgrid = self.setup.zgs_Grids[desc][time]        
+        Vout = valuefunction([grid_dim_linear(agrid),grid_dim_discrete(zgsgrid)],{'V':Vcs[0],'c':Vcs[1],'s':Vcs[2]},time,desc)
+        return Vout
     
-    return u, mu, mu_inv, ue
+
+
+        
+
+
 
 
     
@@ -82,111 +97,38 @@ def define_u(sigma,u_kid_c,u_kid_add):
 if __name__ == "__main__":
     
     
+    # imports
     
-    pars = dict(T = 20,
-                sigma = 1,
-                R = 1.03,
-                beta = 0.97,
-                g_kid = 0.05,
-                z_kid = 0.2,
-                u_kid_c = 0.0,
-                u_kid_add = 0.05,
-                phi_in = 1,
-                phi_out = 0.4,
-                pback = 0.25,
-                eps = 0.00,
-                amax = 40, na = 200,
-                a_z0 = 0.0, a_z1 = 0.05, a_z2 = 0.05,
-                a_g0 = 0.0, a_g1 = -0.1,
-                sigma_z_init=0.15,sigma_z=0.1,nz=7,
-                sigma_g=0.1,rho_g=0.8,ng=7,smax=4.12,ns=16
+    
+    pars_in = dict(
+                    T = 20,
+                    sigma = 1,
+                    R = 1.03,
+                    beta = 0.97,
+                    g_kid = 0.05,
+                    z_kid = 0.2,
+                    u_kid_c = 0.0,
+                    u_kid_add = 0.05,
+                    phi_in = 1,
+                    phi_out = 0.4,
+                    pback = 0.25,
+                    eps = 0.00,
+                    amax = 40, na = 200,
+                    a_z0 = 0.0, a_z1 = 0.05, a_z2 = 0.05,
+                    a_g0 = 0.0, a_g1 = -0.1,
+                    sigma_z_init=0.15,sigma_z=0.1,nz=7,
+                    sigma_g=0.1,rho_g=0.8,ng=7,smax=4.12,ns=16
                 )
     
-    T = pars['T']
-    trange = np.arange(0,pars['T'])
-    trange = trange/np.mean(trange)
-    
-    ztrend = pars['a_z0'] + pars['a_z1']*trange + pars['a_z2']*(trange**2)
-    
-    gtrend = np.exp(pars['a_g0']+ pars['a_g1']*trange)
-    
-    agrid = generate_agrid(n = pars['na'], amax=pars['amax'])  
+    T = pars_in['T']
     
     
+    # set iterator
+    M = model(pars_in,"EGM")
+    M.compute_V()
+    V = M.V
     
-    a = dict(sigma_z_init=pars['sigma_z_init'],sigma_z=pars['sigma_z'],nz=pars['nz'],
-                 sigma_g=pars['sigma_g'],rho_g=pars['rho_g'],ng=pars['ng'],smin=0,smax=pars['smax'],ns=pars['ns'],
-                 T=pars['T'],mult=gtrend)
-    
-    zgs_GridList_nok,   zgs_MatList_nok  = generate_zgs(**a)
-    zgs_GridList_k,     zgs_MatList_k  = generate_zgs(**a,fun = lambda g : np.maximum( g - pars['g_kid'], 0 ) )
-    
-    zgs_GridList = {
-                    'No children': zgs_GridList_nok,
-                    'One child, out': zgs_GridList_k,
-                    'One child, in': zgs_GridList_k
-                   }
-    
-    zgs_MatList  = {
-                    'No children': zgs_MatList_nok,
-                    'One child, out': zgs_MatList_k,
-                    'One child, in': zgs_MatList_k
-                   }
-    
-    iterator = Vnext_egm
-    
-    labor_income = {
-                    'No children':    lambda grid, t : np.exp(grid[:,0] + grid[:,2] + ztrend[t]).T,
-                    'One child, out': lambda grid, t : pars['phi_out']*np.exp(grid[:,0] + grid[:,2] - pars['z_kid']  + ztrend[t]).T,
-                    'One child, in':  lambda grid, t : pars['phi_in']*np.exp(grid[:,0] + grid[:,2]  - pars['z_kid']  + ztrend[t]).T
-                   }
-    
-    u, mu, mu_inv, ue = define_u(pars['sigma'],pars['u_kid_c'],pars['u_kid_add'])
-    
-    
-    V = [{ 'No children':None, 'One child, out':None, 'One child, in':None }]*pars['T']
-    
-    descriptions = [*V[0]] 
-    
-    for desc in descriptions:
-        
-        Vcs = iterator(agrid,labor_income[desc](zgs_GridList[desc][-1],T-1),None,None,None,pars['R'],
-                       pars['beta'],u=u[desc],mu_inv=mu_inv[desc],uefun=ue[desc])  
-        V[T-1][desc] = vpack(Vcs,agrid,zgs_GridList[desc][-1],T-1,desc)
-        
-    for t in reversed(range(T-1)):
-        
-        Vnext = V[t+1]
-        Vcurrent = V[t] # passed by reference
-        
-        for desc in descriptions:   
-            
-            
-            gri, ma = zgs_GridList[desc][t], zgs_MatList[desc][t]            
-            
-            integrate = lambda V : np.dot(V,ma.T)
-            
-            if desc == "One child, in":
-                transition = choice(["One child, in"],pars['eps'])                
-            elif desc == "One child, out":                
-                transition = shock(["One child, out","One child, in"],[1-pars['pback'],pars['pback']])
-            elif desc == "No children":     
-                if_child = shock(["One child, out","One child, in"],[1,0])
-                transition = choice(["No children",if_child],pars['eps'])              
-            else:
-                raise Exception("Unsupported type?")
-                
-                
-            Vcomb, MU_comb = ev_emu(Vnext,transition,mu=mu[desc])
-                
-            assert np.all(MU_comb > 0)            
-            
-            EV, EMU  = integrate(  Vcomb  ), integrate(  MU_comb  )            
-            
-            Vcs = iterator(agrid,labor_income[desc](gri,t),EV,EMU,ma,pars['R'],pars['beta'],u=u[desc],mu_inv=mu_inv[desc],uefun=ue[desc])
-            
-            Vcurrent[desc] = vpack(Vcs,agrid,gri,t,desc)
-        
+    # initialize V
     
     it = 0
     
